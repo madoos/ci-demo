c0a5d6fb9781327486376f4be23d83c0df365583
madoos <maurice.ronet.dominguez@gmail.com>
2017-04-06 18:02:26 +0200

    new feat closes #4

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello CI World + CI test!')
+  res.send('Hello CI World + CI test! feat1')
 })
 
 app.listen(3000, function () {
