73fa8443838d995544fd21edc3ff122105c7c1b7
madoos <maurice.ronet.dominguez@gmail.com>
2017-04-06 15:41:53 +0200

    added CI test closes #1

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello CI World!')
+  res.send('Hello CI World + CI test!')
 })
 
 app.listen(3000, function () {
