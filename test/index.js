f413a109042b5ed88fc98106ddf329059f87131a
madoos <maurice.ronet.dominguez@gmail.com>
2017-04-06 15:05:58 +0200

    Added server demo

--- test/index.js
+++ test/index.js
@@ -3,8 +3,15 @@
 const chai = require('chai')
 const expect = chai.expect
 
+
+const app = require('../index');
+
 describe('Test Hello world', function () {
   it('should to be a string', function () {
       expect('Hello world').to.be.a('string')
+  })
+
+  it('app should to be an object', function () {
+      expect(app).to.be.a('function')
   })
 })
